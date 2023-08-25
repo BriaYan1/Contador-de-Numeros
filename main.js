253ddc67b60727986d73d24641be57e7e310e2c5
Briayan <briayang1997@gmail.com>
2023-08-25 11:24:48 -0400

    Footer V-0.16 - Agregando parrafo de cada servicio

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
  // JavaScript para cerrar el menú después de hacer clic en un enlace
- document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
     const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
     navLinks.forEach(link => {
       link.addEventListener("click", () => {
